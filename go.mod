6ec3fd7508913fa10ee2fd2f10d02ed8b8f6c44d
Trey Tacon <ttacon@gmail.com>
2019-08-29 16:20:49 -0400

    HCL configs

--- go.mod
+++ go.mod
@@ -9,9 +9,14 @@ require (
 	github.com/abrander/go-supervisord v0.0.0-20180808154311-364ce11d29d8 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/flynn-archive/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hpcloud/tail v1.0.0 // indirect
 	github.com/kolo/xmlrpc v0.0.0-20190717152603-07c4ee3fd181 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/tevino/abool v0.0.0-20170917061928-9b9efcf221b5 // indirect
 	github.com/ttacon/pretty v0.0.0-20140822010550-4869e1157de7 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
-	"github.com/BurntSushi/toml"
 	"github.com/abiosoft/ishell"
+	"github.com/hashicorp/hcl"
+	"github.com/hpcloud/tail"
+	"github.com/tevino/abool"
+)
+
+var (
+	configFileLocation = flag.String("config", "glorious.glorious", "config file location")
 )
 
 func main() {
+	flag.Parse()
 
-	config, err := loadConfig()
+	config, err := loadConfig(*configFileLocation)
 	if err != nil {
 		fmt.Println("failed to load config: ", err)
 		os.Exit(1)
@@ -50,6 +61,21 @@ func main() {
 	})
 
 	shell.AddCmd(&ishell.Cmd{
+		Name: "status",
+		Help: "Display status information",
+		Func: func(c *ishell.Context) {
+			c.Printf("%-20s| %-9s\n", "Name", "Status")
+			for _, unit := range config.Units {
+				c.Printf("%-20s| %-9s\n",
+					unit.Name,
+					unit.ProcessStatus(),
+				)
+			}
+
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
 		Name: "start",
 		Help: "Start a given unit",
 		Func: func(c *ishell.Context) {
@@ -66,7 +92,59 @@ func main() {
 
 			for _, unit := range unitsToStart {
 				c.Printf("starting %q...", unit.Name)
+
+				if err := unit.Start(c); err != nil {
+					fmt.Println(err)
+				}
+
 				c.Println("done")
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "stop",
+		Help: "Stops given units",
+		Func: func(c *ishell.Context) {
+			var unitsToStart []*Unit
+			for _, unitName := range c.Args {
+				unit, ok := config.GetUnit(unitName)
+				if !ok {
+					c.Printf("unknown unit %q, aborting\n", unitName)
+					return
+				}
+
+				unitsToStart = append(unitsToStart, unit)
+			}
+
+			for _, unit := range unitsToStart {
+				c.Printf("stopping %q...", unit.Name)
+
+				if err := unit.Stop(c); err != nil {
+					fmt.Println(err)
+				}
+
+				c.Println("done")
+			}
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "tail",
+		Help: "tails a unit",
+		Func: func(c *ishell.Context) {
+			// be cheaper if we can
+			unitName := c.Args[0]
+			unit, ok := config.GetUnit(unitName)
+			if !ok {
+				c.Printf("unknown unit %q, aborting\n", unitName)
+				return
+			}
+
+			// if there are no more args, run a following tail, if
+			// there's a number, run a number sliced tail
+			if err := unit.Tail(c); err != nil {
+				c.Println(err)
 			}
 		},
 	})
@@ -83,14 +161,14 @@ const banner = `       _            _
  |___/
 `
 
-func loadConfig() (*GloriousConfig, error) {
-	data, err := ioutil.ReadFile("./glorious.toml")
+func loadConfig(configFileLocation string) (*GloriousConfig, error) {
+	data, err := ioutil.ReadFile(configFileLocation)
 	if err != nil {
 		return nil, err
 	}
 
 	var m GloriousConfig
-	if err := toml.Unmarshal(data, &m); err != nil {
+	if err := hcl.Unmarshal(data, &m); err != nil {
 		return nil, err
 	}
 
@@ -98,24 +176,180 @@ func loadConfig() (*GloriousConfig, error) {
 }
 
 type GloriousConfig struct {
-	Units []Unit `toml:"unit"`
+	Units []*Unit `hcl:"unit"`
 }
 
 func (g *GloriousConfig) GetUnit(name string) (*Unit, bool) {
 	for _, unit := range g.Units {
 		if unit.Name == name {
-			return &unit, true
+			return unit, true
 		}
 	}
 	return nil, false
 }
 
 type Unit struct {
-	Name        string `toml:"name"`
-	Description string `toml:"description"`
-	Slots       []Slot `toml:"slot"`
+	Name        string `hcl:"name"`
+	Description string `hcl:"description"`
+	Slots       []Slot `hcl:"slot"`
+
+	Status *Status
+}
+
+type UnitStatus int
+
+const (
+	NotStarted UnitStatus = iota
+	Running
+	Stopped
+	Crashed
+)
+
+type Status struct {
+	CurrentStatus UnitStatus
+	Cmd           *exec.Cmd
+	OutFile       *os.File
+
+	shutdownRequested *abool.AtomicBool
+}
+
+func (s Status) String() string {
+	var status string
+	switch s.CurrentStatus {
+	case NotStarted:
+		status = "not started"
+	case Running:
+		status = "running"
+	case Stopped:
+		status = "stopped"
+	case Crashed:
+		status = "crashed"
+	}
+	return status
+}
+
+func (u *Unit) ProcessStatus() string {
+	if u.Status == nil {
+		return "not started"
+	}
+	return u.Status.String()
+}
+
+func (u *Unit) OutputFile() (*os.File, error) {
+	home := os.Getenv("HOME")
+	if len(home) == 0 {
+		return nil, errors.New("cannot determine home directory")
+	}
+
+	outputDir := filepath.Join(home, ".glorious", "output")
+
+	if err := os.MkdirAll(
+		outputDir,
+		0744,
+	); err != nil {
+		return nil, err
+	}
+
+	f, err := os.OpenFile(
+		filepath.Join(outputDir, u.Name),
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	return f, err
+}
+
+func (u *Unit) Start(ctxt *ishell.Context) error {
+	// now for some tomfoolery
+	slot := u.Slots[0]
+	pieces := strings.Split(slot.Cmd, " ")
+	c := exec.Cmd{}
+	c.Dir = slot.WorkingDir
+	c.Path = pieces[0]
+	if len(pieces) > 1 {
+		c.Args = pieces[1:]
+	}
+
+	outputFile, err := u.OutputFile()
+	if err != nil {
+		return err
+	}
+
+	c.Stdout = outputFile
+	c.Stderr = outputFile
+
+	if err := c.Start(); err != nil {
+		return err
+	}
+
+	u.Status = &Status{
+		CurrentStatus: Running,
+		Cmd:           &c,
+		OutFile:       outputFile,
+
+		shutdownRequested: abool.New(),
+	}
+	u.Status.shutdownRequested.UnSet()
+
+	go func(u *Unit) {
+		if err := u.Status.Cmd.Wait(); err != nil {
+			u.Status.CurrentStatus = Crashed
+			u.Status.Cmd = nil
+		}
+
+		u.Status.shutdownRequested.UnSet()
+	}(u)
+
+	ctxt.Printf("begun as pid %d...", c.Process.Pid)
+	return nil
+}
+
+var ErrStopStopped = errors.New("cannot stop stopped unit")
+
+func (u *Unit) Stop(ctxt *ishell.Context) error {
+	if u.Status == nil {
+		return ErrStopStopped
+	}
+
+	u.Status.shutdownRequested.Set()
+
+	if err := u.Status.Cmd.Process.Kill(); err != nil {
+		return err
+	}
+
+	if err := u.Status.OutFile.Close(); err != nil {
+		return err
+	}
+
+	u.Status.Cmd.Stdout = nil
+	u.Status.Cmd.Stderr = nil
+
+	return nil
+}
+
+func (u *Unit) Tail(ctxt *ishell.Context) error {
+
+	t, err := tail.TailFile(u.Status.OutFile.Name(), tail.Config{Follow: true})
+	if err != nil {
+		return err
+	}
+	for line := range t.Lines {
+		ctxt.Println(line.Text)
+	}
+
+	return nil
+
+	/*
+		// let's be cheeky for now and smarter later (implementing `tail`)
+		data, err := exec.Command("tail", "-100", u.Status.OutFile.Name()).Output()
+		if err != nil {
+			return err
+		}
+		ctxt.Println(string(data))
+		return nil
+	*/
 }
 
 type Slot struct {
-	Cmd string `toml:"cmd"`
+	Cmd        string `hcl:"cmd"`
+	WorkingDir string `hcl:"workingDir"`
 }
